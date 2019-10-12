aab14a2a64d440df2e1f8b17089744fd7396b91e
wanghao <www.1246498268@qq.com>
2019-10-12 22:21:52 +0800

    函数练习

---
+++ 10_12.c
@@ -0,0 +1,78 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<math.h>
+//void Jud_prime(int n)
+//{
+//	double k = sqrt(n);
+//	int i = 0;
+//	for (i = 2; i <= k; i++)
+//	{
+//		if (n%i == 0)
+//			break;
+//	}
+//	if (i <= k)
+//		printf("不是素数\n");
+//	else
+//	printf("是素数\n");
+//}
+//int main()
+//{
+//	int n = 0;
+//	printf("请输入一个整数\n");
+//	scanf("%d", &n);
+//	Jud_prime(n);
+//	return 0;
+//}
+//void Change(int arr[3][3])
+//{
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 3; j++)
+//		{
+//			printf("%d ", arr[j][i]);
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int arr[3][3] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//print(arr,sz);
+//	Change(arr);
+//	return 0;
+//}
+//void Parat(int n)
+//{
+//	if (n > 9)
+//	{
+//		Parat(n / 10);
+//	}
+//	printf("%d " ,n%10);
+//}
+//int mian()
+//{
+//	int i = 0;
+//	printf("请输入一个整数：");
+//	scanf("%d", &i);
+//	Parat(i);
+//	return 0;
+//}
+//void Parat(int i)
+//{
+//	if (i > 9)
+//	{
+//		Parat(i / 10);
+//	}
+//	printf("%d ", i%10);
+//}
+//int main()
+//{
+//	int input = 0;
+//	printf("输入一个整数：");
+//	scanf("%d", &input);
+//	Parat(input);
+//	return 0;
+//}
